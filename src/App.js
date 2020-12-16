0d9d1c2edcee26a21626b52b39389faa60d5ff70
Carson <carsonatxmoore@gmail.com>
2020-12-15 19:03:26 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { QueryBuilder } from 'smart-webcomponents-react/querybuilder';
+import 'smart-webcomponents-react/source/styles/components/smart.querybuilder.css';
+import { useState } from 'react'
+
 function App() {
+  const [value, setValue] = useState('programName = "test"');
+
+  const onChange = event => {
+    console.log(event.detail.linq);
+    setValue(event.detail.linq);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryBuilder
+        customOperations={customOperations}
+        fields={fields}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
+
+const fields = [
+  {
+    label: 'Part Number',
+    dataField: 'partNumber',
+    dataType: 'string',
+    filterOperations: ['=', '<>'],
+  },
+  {
+    label: 'Test Program',
+    dataField: 'programName',
+    dataType: 'string',
+    filterOperations: ['=', '<>'],
+  },
+  {
+    label: 'Started At',
+    dataField: 'startedAt',
+    dataType: 'string',
+    filterOperations: ['>'],
+    lookup: {
+      // These don't work
+      dataSource: ['${__from:date}', '${__to:date}', '${__from:date:YYYY-MM-DD}', '${__to:date:YYYY-MM-DD}'],
+    }
+  },
+  {
+    label: 'Status',
+    dataField: 'status.statusType',
+    dataType: 'string',
+    filterOperations: ['=', '<>'],
+    lookup: {
+      dataSource: [
+        // this doesn't work
+        { label: 'Fancy label', value: 'uglyValue' },
+      ],
+      readonly: true
+    }
+  },
+  {
+    label: 'Operator',
+    dataField: 'operator',
+    dataType: 'string',
+    filterOperations: ['=', '<>'],
+    // this doesn't work :(
+    lookup: {
+      dataSource: (query) => {
+        return new Promise(resolve => {
+          setTimeout(() => resolve(['cat', 'dog', 'zebra']));
+        })
+      }
+    }
+  },
+];
+
+const customOperations = [
+  {
+    label: 'Equals',
+    name: '=',
+    expressionTemplate: '{0} = "{1}"',
+  },
+  {
+    label: 'Does not equal',
+    name: '<>',
+    expressionTemplate: '{0} != "{1}"',
+  },
+  {
+    label: 'Greater than',
+    name: '>',
+    expressionTemplate: '{0} > {1}',
+  },
+];
 
 export default App;
