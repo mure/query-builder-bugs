958775356a9fd91a995468c3f1689e06b34c8db6
mure <carsonatxmoore@gmail.com>
2021-02-09 15:57:48 -0600

    New bug

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import './App.css';
 
 import { QueryBuilder } from 'smart-webcomponents-react/querybuilder';
 import 'smart-webcomponents-react/source/styles/components/smart.querybuilder.css';
-import { useState } from 'react'
 
 function App() {
-  const [value, setValue] = useState('programName = "test"');
-
   const onChange = event => {
     console.log(event.detail.linq);
-    setValue(event.detail.linq);
   }
 
   return (
     <div className="App">
       <QueryBuilder
         customOperations={customOperations}
+        getDynamicField={() => ({filterOperations: ['propertyequals']})}
         fields={fields}
-        value={value}
+        value={'partNumber = "123" && properties["dog"] = "cat"'}
         onChange={onChange}
       />
     </div>
@@ -29,50 +26,7 @@ const fields = [
     label: 'Part Number',
     dataField: 'partNumber',
     dataType: 'string',
-    filterOperations: ['=', '<>'],
-  },
-  {
-    label: 'Test Program',
-    dataField: 'programName',
-    dataType: 'string',
-    filterOperations: ['=', '<>'],
-  },
-  {
-    label: 'Started At',
-    dataField: 'startedAt',
-    dataType: 'string',
-    filterOperations: ['>'],
-    lookup: {
-      // These don't work
-      dataSource: ['${__from:date}', '${__to:date}', '${__from:date:YYYY-MM-DD}', '${__to:date:YYYY-MM-DD}'],
-    }
-  },
-  {
-    label: 'Status',
-    dataField: 'status.statusType',
-    dataType: 'string',
-    filterOperations: ['=', '<>'],
-    lookup: {
-      dataSource: [
-        // this doesn't work
-        { label: 'Fancy label', value: 'uglyValue' },
-      ],
-      readonly: true
-    }
-  },
-  {
-    label: 'Operator',
-    dataField: 'operator',
-    dataType: 'string',
-    filterOperations: ['=', '<>'],
-    // this doesn't work :(
-    lookup: {
-      dataSource: (query) => {
-        return new Promise(resolve => {
-          setTimeout(() => resolve(['cat', 'dog', 'zebra']));
-        })
-      }
-    }
+    filterOperations: ['='],
   },
 ];
 
@@ -83,15 +37,10 @@ const customOperations = [
     expressionTemplate: '{0} = "{1}"',
   },
   {
-    label: 'Does not equal',
-    name: '<>',
-    expressionTemplate: '{0} != "{1}"',
-  },
-  {
-    label: 'Greater than',
-    name: '>',
-    expressionTemplate: '{0} > {1}',
-  },
+    label: 'Equals',
+    name: 'propertyequals',
+    expressionTemplate: 'properties["{0}"] = "{1}"',
+  }
 ];
 
 export default App;
